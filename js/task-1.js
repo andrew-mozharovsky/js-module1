c668d5c7e6336413cc51d3e67822ff77f2349d32
AndrewMozharovsky <71605765+AndrewMozharovsky@users.noreply.github.com>
2020-12-27 21:30:57 +0200

    27

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+// Пиши код ниже этой строки
+const productName = 'Дроид'
+const pricePeritem = 2000
+
+  
+console.log(productName);
+// 'Дроид'
+
+console.log(pricePerItem);
+// 2000
---
+++ js/task-2.js
@@ -0,0 +1,8 @@
+// Базовый код
+let productName = 'Дроид';
+let pricePerItem = 2000;
+
+// Пиши код ниже этой строки
+
+productName = 'Ремонтный дроид'
+pricePerItem = 3500
